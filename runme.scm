fb44b28be1a4a5272b3ae504750e925f654f5555
midori <cleoold@gmail.com>
2019-05-18 18:14:39 +0800

    Update runme.scm

--- runme.scm
+++ runme.scm
@@ -10,5 +10,9 @@
                 (exn:fail:syntax?
                  (lambda (e) (display "SYNTAX FAIL: be sure the variables are precisely as required.\n\n")
                    (display "the programme is recovered from a syntax failure error.\n")
+                   (caltool-main)))
+                (exn:fail:read?
+                 (lambda (e) (display "READ FAIL: note enclosing brackets and prefix notion.\n\n")
+                   (display "the programme is recovered from a read failure error.\n")
                    (caltool-main))))
 (caltool-main))
