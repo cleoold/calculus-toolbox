0c7ddec3e7d7fac38b48c2e043789f2070d608c5
midori <cleoold@gmail.com>
2019-08-05 21:35:06 +0800

    slight change to derivative algorithm

--- library/cal-library/derivative-single-var.scm
+++ library/cal-library/derivative-single-var.scm
@@ -5,12 +5,29 @@
 (provide right-derivative
          lr-second-derivative)
 
+;; helper
+(define (halving-loop algo f x0 res step prec)
+  (define newres (algo f x0 step))
+  (cond ((< (abs (- res newres)) step)
+           newres)
+          (else
+           (halving-loop algo f x0 newres (/ step 2) prec))))
 
-(define (right-derivative f x0 step)
+;;==========================
+
+(define (right-derivative-algo f x0 step)
   (/ (- (f (+ x0 step)) (f x0)) step))
 
+(define (right-derivative f x0 prec)
+  (halving-loop right-derivative-algo f x0 0 1 prec))
 
-(define (lr-second-derivative f x0 step)
+
+
+
+(define (lr-second-derivative-algo f x0 step)
   (define l (/ (- (f x0) (f (- x0 step))) step))
-  (define r (right-derivative f (+ x0 step) step))
+  (define r (right-derivative-algo f (+ x0 step) step))
   (/ (- r l) (* 2 step)))
+
+(define (lr-second-derivative f x0 prec)
+  (halving-loop lr-second-derivative-algo f x0 0 1 prec))
